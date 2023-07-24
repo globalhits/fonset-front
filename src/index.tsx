ec71fc5d887779eedc0f27db3dd720a537a2af02
samir26-05 <saorozco2605@gmail.com>
2023-07-24 11:11:09 -0500

    template admin

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { store } from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
  
 
-const root = ReactDOM.createRoot(
+const root = ReactDOM.createRoot( 
 	document.getElementById('root') as HTMLElement
 );
 root.render(
