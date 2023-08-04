c54813717022602377e882a1e598141e0de957db
jordanglobal1626 <orozcojo@globalhitss.com>
2023-08-04 14:21:03 -0500

    Nuevos ajustes

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,17 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 
-import  LoadingReducer  from './states/loading.slice';
+import LoadingReducer from './states/loading.slice';
+import DepartamentReducer from './states/generals/departament.slice';
 
 export const store = configureStore({
-    reducer: {
+	reducer: {
 		loading: LoadingReducer,
-    }
+		departament: DepartamentReducer,
+	}
 })
 
 export type AppDispatch = typeof store.dispatch;
-export type RootState =  ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<
 	ReturnType,
 	RootState,
