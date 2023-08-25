bddd7c029e3d6aa4404bf9bff25fc5c6ed60ac8f
samir26-05 <saorozco2605@gmail.com>
2023-08-25 12:50:44 -0500

    pruebas

--- src/components/templates/adminLte/sidebar/index.tsx
+++ src/components/templates/adminLte/sidebar/index.tsx
@@ -8,13 +8,13 @@ const Sidebar = () => {
 		<aside className="main-sidebar sidebar-dark-primary elevation-4">
 			{/* Brand Logo */}
 			<a href="/" className="brand-link text-center">
-			{/* 	<span className="brand-text font-weight-light">GESTOR DE PROYECTOS</span>
-			 */}</a>
+				<span className="brand-text font-weight-light">GESTOR DE PROYECTOS</span>
+			</a>
 			<div className="sidebar">
 				<div className="user-panel mt-3 pb-3 mb-3 d-flex">
 					<div className="image">
-					{/* 	<img src="/images/logo-vertical.png" alt="Logo de la fiscalia" className="brand-image elevation-3" style={{ opacity: ".8", width: "120px", height: "150px", marginLeft: "50px" }} />
-					 */}</div>
+						<img src="/images/logo-vertical.png" alt="Logo de la fiscalia" className="brand-image elevation-3" style={{ opacity: ".8", width: "120px", height: "150px", marginLeft: "50px" }} />
+					</div>
 				</div>
 				{/* Sidebar Menu */}
 				<nav className="mt-2">
