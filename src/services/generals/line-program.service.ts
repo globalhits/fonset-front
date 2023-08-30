90739bcb9e17eb30c85fae129051d3833f5dc52b
jordanglobal1626 <orozcojo@globalhitss.com>
2023-08-30 03:28:20 -0500

    CONST list programs and lines

--- src/services/generals/line-program.service.ts
+++ src/services/generals/line-program.service.ts
@@ -1,18 +1,18 @@
-import config from "../../config/config";
-// import { LIST_LINES_PROGRAMS } from "../../config/constants";
+// import config from "../../config/config";
+import { LIST_LINES_PROGRAMS } from "../../config/constants";
 
 class LineProgramService {
 
-    private apiGenerals = config.apiGeneralHost;
+    // private apiGenerals = config.apiGeneralHost;
 
-    private pathListLineProgram = config.pathListLineProgram;
+    // private pathListLineProgram = config.pathListLineProgram;
 
     async getAll() {
         try {
-            const response = await fetch(`${this.apiGenerals}/${this.pathListLineProgram}`);
-            const data = await response.json();
-            return data;
-            // return LIST_LINES_PROGRAMS;
+            // const response = await fetch(`${this.apiGenerals}/${this.pathListLineProgram}`);
+            // const data = await response.json();
+            // return data;
+            return LIST_LINES_PROGRAMS;
         } catch (error) {
             console.error('Error fetching data:', error);
             throw error;
--- src/services/generals/program.service.ts
+++ src/services/generals/program.service.ts
@@ -1,18 +1,18 @@
-import config from "../../config/config";
-// import { LIST_PROGRAMS } from "../../config/constants";
+// import config from "../../config/config";
+import { LIST_PROGRAMS } from "../../config/constants";
 
 class ProgramService {
 
-    private apiGenerals = config.apiGeneralHost;
+    // private apiGenerals = config.apiGeneralHost;
 
-    private pathListProgram = config.pathListProgram;
+    // private pathListProgram = config.pathListProgram;
 
     async getAll() {
         try {
-            const response = await fetch(`${this.apiGenerals}/${this.pathListProgram}`);
-            const data = await response.json();
-            return data;
-            // return LIST_PROGRAMS;
+            // const response = await fetch(`${this.apiGenerals}/${this.pathListProgram}`);
+            // const data = await response.json();
+            // return data;
+            return LIST_PROGRAMS;
         } catch (error) {
             console.error('Error fetching data:', error);
             throw error;
